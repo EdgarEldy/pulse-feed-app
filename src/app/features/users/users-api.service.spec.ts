711b5baa036903390882ae378dfced85b6e2a8be
EdgarEldy <ed.eldy21@live.com>
2026-09-12 16:51:06 +0400

    test(users): provide TranslateService in UsersApiService's TestBed

--- src/app/features/users/users-api.service.spec.ts
+++ src/app/features/users/users-api.service.spec.ts
@@ -3,6 +3,7 @@ import { HttpTestingController, provideHttpClientTesting } from '@angular/common
 import { TestBed } from '@angular/core/testing';
 import { apiEndpoints } from '../../core/http/api-endpoints';
 import { environment } from '../../../environments/environment';
+import { provideTestTranslations } from '../../testing/translate-testing';
 import { UsersApiService } from './users-api.service';
 
 const sampleUserDto = {
@@ -19,7 +20,7 @@ describe('UsersApiService', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [provideHttpClient(), provideHttpClientTesting()],
+      providers: [provideHttpClient(), provideHttpClientTesting(), provideTestTranslations()],
     });
 
     service = TestBed.inject(UsersApiService);
